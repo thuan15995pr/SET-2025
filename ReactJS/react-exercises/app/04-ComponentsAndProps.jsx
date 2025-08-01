75c663f1353f543a798dcc7bf5934793d94f56b0
Duy Thuan <boooyoung152@gmail.com>
2025-08-01 20:14:11 +0700

    Fix bug

--- ReactJS/react-exercises/app/04-ComponentsAndProps.jsx
+++ ReactJS/react-exercises/app/04-ComponentsAndProps.jsx
@@ -8,7 +8,6 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-// ES6 Class component (legacy but still used)
 class WelcomeClass extends React.Component {
   render() {
     return <h1>Hello, {this.props.name}</h1>;
